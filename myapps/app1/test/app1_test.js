3daf2b012e71fab9f2a32ea2336bb1799f7034e0
Gao Liang <liang.gao@rea-group.com>
2016-01-28 23:09:49 +0800

    add test for using button CK200

--- myapps/app1/test/app1_test.js
+++ myapps/app1/test/app1_test.js
@@ -10,11 +10,39 @@ var verify = mock.verify;
 var appPath = path.join(__dirname, '..');
 
 module.exports = {
-    'test should run application': function () {
+//    'test should run application': function () {
+//        appRunner
+//            .run(appPath, function () {
+//                $('button').emit('push');
+//                verify($('led-r')).turnOn();
+//
+//                $('button').emit('release');
+//                verify($('led-r')).turnOff();
+//            })
+//            .end(function () {
+////                verify($('led-r')).turnOff();
+//            });
+//    }
+
+    'test should trun on led if button pressed': function () {
         appRunner
             .run(appPath, function () {
+                $('button').emit('push');
                 verify($('led-r')).turnOn();
-            })
+            });
+    },
+
+    'test should trun off led if button released': function () {
+        appRunner
+            .run(appPath, function () {
+                $('button').emit('release');
+                verify($('led-r')).turnOff();
+            });
+    },
+
+    'test should trun off led if app is stopped': function () {
+        appRunner
+            .run(appPath, function() {})
             .end(function () {
                 verify($('led-r')).turnOff();
             });
