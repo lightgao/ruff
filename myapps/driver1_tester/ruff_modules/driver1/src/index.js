2b5043ad967da3e33254b0273f23cf75b3ec954e
Gao Liang <liang.gao@rea-group.com>
2016-01-30 15:08:32 +0800

    remove useless code in driver1

--- myapps/driver1_tester/ruff_modules/driver1/src/index.js
+++ myapps/driver1_tester/ruff_modules/driver1/src/index.js
@@ -3,16 +3,6 @@
 var driver = require('ruff-driver');
 
 module.exports = driver({
-    attach: function (inputs) {
-        //do init steps here
-        //this._gpio = inputs.getRequired('gpio');
-    },
-
-    detach: function () {
-        //do clean work here
-    },
-
-
     attach: function(inputs) {
         this._mygpio = inputs.getRequired('mygpio');
     },
